228309915326ab133f0f00cb22f8530a91446a4e
xlinlin23 <xiaolinjiang6@gmail.com>
2021-03-14 18:02:17 -0700

    command line

--- CSRSeq.c
+++ CSRSeq.c
@@ -1,5 +1,4 @@
 #include "CSR.h"
-#define ITERATION 1
 double* multiplication(struct CSR matrix, double* x, int column_size){
     double* result = malloc(column_size*sizeof(double));
     for(int i = 0; i < column_size; i++){
@@ -10,8 +9,18 @@ double* multiplication(struct CSR matrix, double* x, int column_size){
     return result;
 }
 
-int main(){
-    char* filename = "matrix1.txt";
+int main(int argc, char *argv[]){
+    //./spmv matrix.rft m n
+    if(argc != 4){
+        printf("error input!");
+        exit(0);
+    }
+    char* filename;
+    int ITERATIONS = 0;
+    int NUMBER_THREADS = 0;
+        filename = argv[1];
+        ITERATIONS = atoi(argv[2]);
+        NUMBER_THREADS = atoi(argv[3]);
     struct CSR _matrix = convertCSR(filename);
     int _column_size = _matrix.column_size;
     double* x = malloc(_matrix.column_size*sizeof(double));
@@ -19,10 +28,10 @@ int main(){
         x[i] = 1;
     }
 
-    for(int i = 1; i <= ITERATION; i++){
+    for(int i = 1; i <= ITERATIONS; i++){
         x = multiplication(_matrix, x, _column_size);
         char outfile_name[256];
-        sprintf(outfile_name, "CSRVec%d", i);
+        sprintf(outfile_name, "CSR2Vec%d", i);
         FILE *fp;
         fp = fopen(outfile_name, "w");
         for(int k = 0; k < _column_size; k++)
